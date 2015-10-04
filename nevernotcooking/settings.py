16700bc2248526f81f85bc920c84aeea1b7c46b7
wabarr <wabarr@gmail.com>
2015-10-04 14:33:49 -0400

    make homepage a manageable page

--- nevernotcooking/settings.py
+++ nevernotcooking/settings.py
@@ -183,11 +183,11 @@ STATIC_ROOT = "/home/wabarr/webapps/cookingstatic/"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = STATIC_URL + "media/"
+MEDIA_URL = "/media/"
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+MEDIA_ROOT = "/home/wabarr/webapps/cookingmedia/"
 
 # Package/module name to import the root urlpatterns from for the project.
 ROOT_URLCONF = "%s.urls" % PROJECT_APP
